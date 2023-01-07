afdd2b865e6fdc3d8e3fca068c0b47ddbe9f02ad
a-lil-birb <48155605+a-lil-birb@users.noreply.github.com>
2023-01-07 15:33:28 -0500

    Add speed boost

---
+++ GameDev/Assets/Scripts/BoostSpeed.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class BoostSpeed : MonoBehaviour {
+	
+	bool triggered = false;
+
+	// Before rendering each frame..
+	void Update () 
+	{
+		// Rotate the game object that this script is attached to by 15 in the X axis,
+		// 30 in the Y axis and 45 in the Z axis, multiplied by deltaTime in order to make it per second
+		// rather than per frame.
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+
+    IEnumerator boostPlayerSpeed(GameObject player)
+    {
+		NetworkCharacterControllerPrototype characterController = player.GetComponent<NetworkCharacterControllerPrototype>();
+        characterController.maxSpeed = 50f;
+        characterController.acceleration = 50f;
+        yield return new WaitForSeconds(20);
+        characterController.maxSpeed = 4f;
+        characterController.acceleration = 10f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!triggered & other.gameObject.CompareTag("Player"))
+        {
+			triggered = true;
+            StartCoroutine(boostPlayerSpeed(other.gameObject));
+			gameObject.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
