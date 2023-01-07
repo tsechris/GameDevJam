b2a5b218f755319789d27b514c0b7e870cae23d9
a-lil-birb <48155605+a-lil-birb@users.noreply.github.com>
2023-01-07 18:08:27 -0500

    Tween library

--- GameDev/Assets/Scripts/BoostSizeUp.cs
+++ GameDev/Assets/Scripts/BoostSizeUp.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class BoostSizeUp : MonoBehaviour {
 	
@@ -16,22 +17,10 @@ public class BoostSizeUp : MonoBehaviour {
 
     IEnumerator flashGrowth(GameObject player)
     {
-		int max = 8;
-        for (int i=0; i<max; i++)
-        {
-            player.transform.localScale = new Vector3(i/3f+1,i/3f+1,i/3f+1);
-            yield return new WaitForSeconds(0.1f);
-            player.transform.localScale = new Vector3(i/3f+2,i/3f+2,i/3f+2);
-            yield return new WaitForSeconds(0.1f);
-        }
+        float originalScale = player.transform.localScale.x;
+		player.transform.DOScale(originalScale + 3f, 1f);
         yield return new WaitForSeconds(5);
-        for (int i=0; i<max; i++)
-        {
-            player.transform.localScale = new Vector3((max-i)/3+2,(max-i)/3+2,(max-i)/3+2);
-            yield return new WaitForSeconds(0.1f);
-            player.transform.localScale = new Vector3((max-i)/3+1,(max-i)/3+1,(max-i)/3+1);
-            yield return new WaitForSeconds(0.1f);
-        }
+        player.transform.DOScale(originalScale, 1f);
     }
 
     private void OnTriggerEnter(Collider other)
