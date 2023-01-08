926d3e1092940ee3ad8131336f3bf0027ba0bd2a
NikolasPasichnik <nikolas.pasichnik@mail.mcgill.ca>
2023-01-07 20:41:26 -0800

    Finished the Booster Shop UI

---
+++ GameDev/Assets/Scripts/SkillShopScripts/BoosterShopExitScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterShopExitScript : MonoBehaviour
+{
+    // Change the scene here
+    public void ButtonPressExit(){
+        Debug.Log("Returning to main menu"); 
+    }
+}
