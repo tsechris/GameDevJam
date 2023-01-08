a7e1c8b60460628f8b67802e9f7e353f33ee5e64
Niels <77604434+nielsmai@users.noreply.github.com>
2023-01-07 19:42:10 -0500

    Hid test GUI

--- GameDev/Assets/BasicSpawner.cs
+++ GameDev/Assets/BasicSpawner.cs
@@ -118,20 +118,20 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
 
   private NetworkRunner _runner;
 
-  private void OnGUI()
-  {
-    if (_runner == null)
-    {
-      if (GUI.Button(new Rect(0,0,200,40), "Host"))
-      {
-          StartGame(GameMode.Host);
-      }
-      if (GUI.Button(new Rect(0,40,200,40), "Join"))
-      {
-          StartGame(GameMode.Client);
-      }
-    }
-  }
+  // private void OnGUI()
+  // {
+  //   if (_runner == null)
+  //   {
+  //     if (GUI.Button(new Rect(0,0,200,40), "Host"))
+  //     {
+  //         StartGame(GameMode.Host);
+  //     }
+  //     if (GUI.Button(new Rect(0,40,200,40), "Join"))
+  //     {
+  //         StartGame(GameMode.Client);
+  //     }
+  //   }
+  // }
 
   private void Start() {
     hostButton.onClick.AddListener(() => {
